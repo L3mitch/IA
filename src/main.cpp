ce398331e282034ee3fa366f836de7e42c13523d
Lemitch <michel.almeidadarosa@gmail.com>
2025-06-25 17:09:45 -0300

    versão estável mas com dados teste

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,91 @@
-#include <Arduino.h>
-#include "DHTesp.h"
-#include <IRremoteESP8266.h>
-#include <IRrecv.h>
-#include <IRutils.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <ArduinoJson.h>
 
-#define LED_INDICADOR 22
-#define DHT22_PIN 21
-#define BUZZER 23
-#define UMID_SOLO 4
-#define IR_RECEIVER 0
+// Credenciais do Wi-Fi
+const char* ssid = "Kafei";
+const char* password = "kau15092003@CASA621";
 
-IRrecv irrecv(IR_RECEIVER);
-decode_results results;
+// Configuração de IP fixo
+IPAddress local_IP(192, 168, 7, 123);
+IPAddress gateway(192, 168, 7, 1);
+IPAddress subnet(255, 255, 255, 0);
 
-DHTesp dhtSensor;
+// Servidor na porta 80
+WebServer server(80);
 
-enum IRButtonStates
-{
-  POWER = 162,
-  MODO = 226,
-  PLAY = 168,
-  CIMA = 2,
-  BAIXO = 152,
-  ESQUERDA = 224,
-  DIREITA = 144,
-};
+// Dados simulados
+const char* horarios[] = {"12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00"};
+int umidades[] =    {80, 60, 32, 10, 100, 90, 70};
+float temperaturas[] = {25.0, 26.1, 27.0, 27.0, 26.2, 24.7, 22.3};
 
+// Rota de umidade
+void handleUmidade() {
+  DynamicJsonDocument doc(1024);
+  JsonArray root = doc.to<JsonArray>();
 
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(BUZZER, OUTPUT);
-  pinMode(LED_INDICADOR, OUTPUT);
-  pinMode(IR_RECEIVER, INPUT_PULLDOWN);
-  dhtSensor.setup(DHT22_PIN, DHTesp::DHT22);
-  irrecv.enableIRIn(); // Inicia o receptor IR
-  delay(250);
-  Serial.println("------------------------------------------------");
-  Serial.println("Iniciando o sistema...");
-  Serial.println("------------------------------------------------");
+  JsonArray header = root.createNestedArray();
+  header.add("Horário");
+  header.add("Umidade");
 
-  delay(250);
+  for (int i = 0; i < 7; i++) {
+    JsonArray linha = root.createNestedArray();
+    linha.add(horarios[i]);
+    linha.add(umidades[i]);
+  }
+
+  String json;
+  serializeJson(doc, json);
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "application/json", json);
 }
 
-void loop()
-{
+// Rota de temperatura
+void handleTemperatura() {
+  DynamicJsonDocument doc(1024);
+  JsonArray root = doc.to<JsonArray>();
+
+  JsonArray header = root.createNestedArray();
+  header.add("Horário");
+  header.add("Temperatura");
+
+  for (int i = 0; i < 7; i++) {
+    JsonArray linha = root.createNestedArray();
+    linha.add(horarios[i]);
+    linha.add(temperaturas[i]);
+  }
+
+  String json;
+  serializeJson(doc, json);
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "application/json", json);
+}
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+    Serial.println("Falha ao configurar IP fixo!");
+  }
+
+  WiFi.begin(ssid, password);
+  Serial.print("Conectando ao Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWi-Fi conectado!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());
+
+  // Inicia rotas
+  server.on("/umidade", handleUmidade);
+  server.on("/temperatura", handleTemperatura);
+  server.begin();
+  Serial.println("Servidor HTTP iniciado");
+}
+
+void loop() {
+  server.handleClient();
 }
